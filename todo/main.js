5f23a26440373e9effbb01baa2d8b56f8645d73f
GBAlvim <gabrielroberto523@gmail.com>
2024-04-18 20:48:14 -0300

    Atual. to do list, função 'enter'

--- todo/main.js
+++ todo/main.js
@@ -6,10 +6,40 @@ function addTask() {
         var todoList = document.getElementById("todoList");
         var li = document.createElement("li");
         li.textContent = taskText;
-        todoList.appendChild(li);
-        taskInput.value = "";
+        todoList.appendChild(li)
+        taskInput.value = "";        
     }
 }
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+var draggedElement;
+
+function dragStart(event) {
+    draggedElement = event.target;
+}
+
+function drop(event, targetId) {
+    event.preventDefault();
+    var targetList = document.getElementById(targetId + "List");
+    targetList.appendChild(draggedElement);
+}
+
+var taskLists = document.querySelectorAll('.task-list ul');
+taskLists.forEach(function(list) {
+    list.addEventListener('dragstart', dragStart);
+});
+
+// Adiciona um event listener para a tecla Enter
+document.getElementById("taskInput").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+
+
 
 function allowDrop(event) {
     event.preventDefault();
